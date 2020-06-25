da561301477adb50ea1b6f7aac57b3e3bec2dd52
Norserium <norserium@yandex.ru>
2020-06-26 00:25:30 +0300

    Prevent the filling canvas by black color

--- src/Cropper.vue
+++ src/Cropper.vue
@@ -449,7 +449,7 @@ export default {
 			canvas.height = coordinates.height;
 
 			const ctx = canvas.getContext('2d');
-
+			ctx.clearRect(0,0,canvas.width,canvas.height);
 			ctx.drawImage(
 				source,
 				coordinates.left,
