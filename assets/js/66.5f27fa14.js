1b2be87da6526ba3802a25564b9cc5f1f30906e7
Norserium <norserium@yandex.ru>
2021-01-22 16:09:14 +0300

    Updates

--- assets/js/66.5f27fa14.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[66],{310:function(t,n,s){},315:function(t,n,s){"use strict";s(310)},320:function(t,n,s){"use strict";s.r(n);s(315);var e=s(41),i=Object(e.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"vertical-buttons"},[this._t("default")],2)}),[],!1,null,null,null);n.default=i.exports}}]);
