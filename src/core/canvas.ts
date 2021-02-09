ed4d97f2beb583bd583909325171540e8e0eeac1
Norserium <norserium@yandex.ru>
2021-02-09 11:16:32 +0300

    Clean the canvas related code

--- src/core/canvas.ts
+++ src/core/canvas.ts
@@ -4,10 +4,9 @@ export function updateCanvas(
 	canvas: HTMLCanvasElement,
 	source: HTMLCanvasElement | HTMLImageElement,
 	coordinates: Coordinates,
-	resultCoordinates?: Coordinates,
 ) {
-	canvas.width = resultCoordinates ? resultCoordinates.width : coordinates.width;
-	canvas.height = resultCoordinates ? resultCoordinates.height : coordinates.height;
+	canvas.width = coordinates.width;
+	canvas.height = coordinates.height;
 
 	const ctx = canvas.getContext('2d');
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
